3a8b15e9358a9655794f224b2c12dd47ec8700c5
yuki shimizu <yuki@shimizu-no-MacBook-Pro.local>
2015-09-04 12:52:29 +0900

    メール認証修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,16 +40,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  #メール認証の設定
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => 'smtp.gmail.com',
-    :user_name => "taskonrails@gmail.com", #gmailアドレス
-    :password => "Zerugei99", #パスワード
-    :authentication => 'login',
-  }
+  
 end
