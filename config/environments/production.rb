789ddf39203cf95dbdbd4c6e1849b943fddbc935
yuki shimizu <yuki@shimizu-no-MacBook-Pro.local>
2015-10-14 00:01:39 +0900

    update drag and drop

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,8 @@ Rails.application.configure do
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => 'smtp.gmail.com',
+    :user_name => 'taskonrails', #gmailアドレス
+    :password => 'lrnftrabexbohzxy', #アプリ固有のパスワード
 
     :authentication => 'login',
   }
